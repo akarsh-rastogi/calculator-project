2e7f658925e16f9aed4b24bf6d8e9ab416383d64
akarsh-rastogi <akarshrastogi60@gmail.com>
2025-02-02 13:09:37 +0530

    commit in one line

--- text.js
+++ text.js
@@ -2,8 +2,7 @@ let input = document.getElementById("inputbox");
 let buttons = document.querySelectorAll(".button");
 
 let string = "";
-let arr = Array.from(buttons);
-
+// let arr = Array.from(buttons);
 
 arr.forEach((button) => {
   button.addEventListener("click", (e) => {
